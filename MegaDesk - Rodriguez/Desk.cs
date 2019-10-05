b6b04f8ac7f68acd0912d44655cf9696458fa8bf
Oswaldo Rodriguez <Owner2303@gmail.com>
2019-10-05 10:09:45 -0400

    Fixing Class Desk

--- MegaDesk - Rodriguez/Desk.cs
+++ MegaDesk - Rodriguez/Desk.cs
@@ -6,38 +6,44 @@ using System.Threading.Tasks;
 
 namespace MegaDesk___Rodriguez
 {
+
     class Desk
     {
-        enum surfaceMaterials
-        {
-            Oak = 200,
-            Laminate = 100,
-            Pine = 50,
-            Rosewood = 300,
-            Veneer = 125
-        };
+        double width { get; set; }
+        double depth { get; set; }
+        int numberDrawer { get; set; }
+        DesktopMaterials SurfaceMaterial { get; set; }
 
-        double width, depth;
-        int numberDrawer;
-        int surfaceMaterial;
-
-        public static void getDesk(double width,
-                    double depth,
-                    int numberDrawer,
-                    int surfaceMaterial)
-        {
-
-            Console.WriteLine("type in Desk Width in inches");
-            width = double.Parse(Console.ReadLine());
-            Console.WriteLine("type in Desk depth in inches");
-            depth = double.Parse(Console.ReadLine());
-            Console.WriteLine("type in the number of drawers you want your desk with");
-            numberDrawer = int.Parse(Console.ReadLine());
-            Console.WriteLine("Select the desire Surface Material");
-            Console.WriteLine("1] laminate - 2]oak - 3] rosewood - 4] veneer - 5] pine");
-
-            surfaceMaterial = int.Parse(Console.ReadLine());
-        }
-           
     }
+    enum DesktopMaterials
+    {
+        Oak = 200,
+        Laminate = 100,
+        Pine = 50,
+        Rosewood = 300,
+        Veneer = 125
+    };
+    /*
+    public static void getDesk(double width,
+                double depth,
+                int numberDrawer,
+                int surfaceMaterial
+        )
+    {
+
+        Console.WriteLine("type in Desk Width in inches");
+        width = double.Parse(Console.ReadLine());
+        Console.WriteLine("type in Desk depth in inches");
+        depth = double.Parse(Console.ReadLine());
+        Console.WriteLine("type in the number of drawers you want your desk with");
+        numberDrawer = int.Parse(Console.ReadLine());
+        Console.WriteLine("Select the desire Surface Material");
+        Console.WriteLine("1] laminate - 2]oak - 3] rosewood - 4] veneer - 5] pine");
+
+        surfaceMaterial = int.Parse(Console.ReadLine());
+
+    }     
+}*/
+
+
 }
